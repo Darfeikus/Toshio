8629f1c74ae60d4f2056dc8ef60e16ec7365abf7
AndreRdz7 <A01730716@itesm.mx>
2020-05-09 20:17:26 -0500

    ps no se mijos aiura, no quiere jalar el pendejo bustra

--- online-grader/src/app/dashboard/dashboard.module.ts
+++ online-grader/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HomeComponent } from "./home/home.component";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule, DashboardRoutingModule]
 })
 export class DashboardModule {}
