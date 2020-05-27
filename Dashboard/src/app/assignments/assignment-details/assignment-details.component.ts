3b7d4cb637e5eb8f5bd487174b441f8c9a3a84f1
AndreRdz7 <A01730716@itesm.mx>
2020-05-27 15:28:14 -0500

    previo al file viewer

---
+++ Dashboard/src/app/assignments/assignment-details/assignment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-details',
+  templateUrl: './assignment-details.component.html',
+  styleUrls: ['./assignment-details.component.css']
+})
+export class AssignmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
